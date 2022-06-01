3621c25f4b31fc6cb0f2b133bd1305c9730d7e3b
Rohan Manjunath Ashlesh <rashlesh@deloitte.com>
2022-06-01 12:03:07 +0530

    init commit

---
+++ app/src/main/java/com/ashlesh/owlclone/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.ashlesh.owlclone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ashlesh.owlclone.ui.theme.OwlCloneTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            OwlCloneTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    OwlCloneTheme {
+        Greeting("Android")
+    }
+}
